c45a05be493e5cdb587371c347b45e2df3ed7b43
nitroqwe2010 <nitroasd71@gmail.com>
2024-12-26 05:31:35 -0500

    sa

---
+++ main.js
@@ -0,0 +1,21 @@
+// let str = prompt('String tipida malumot kiriting');
+// let number  = prompt('Number tipida malumot kiriting');
+// let boolean = prompt('Boolean tipda malumot kiriting');
+
+// confirm('Malumotlari togri kiritingizmi ?');
+
+// alert(`Siz kiritgan ${str} sozi string tipga tegishli \n Siz kiritgan ${number} Number tipga tegishli \n Siz kiritgan ${boolean} sozi boolean tipga tegishli`)
+
+
+// let random = Math.random()*100
+
+let son = Math.round(Math.random()*100)
+alert(`sirnig random raqamingiaz ${son}`)
+
+alert (`siz songizni 2ga kopaytirib bolmoq chimisiz`)
+
+let ikkiga = son / 2
+let ikkigak = son * 2
+alert(`sirnig random raqamingiaz ${son} \n sisani sonig ni ikkiga bolganda chiqan javob ${ikkiga} \nsani sonig ni ikkiga kopaytirganda chiqan javob ${ikkigak} \n qoldiqni ozin hisob la`)
+
+// alert(`sani sonig ni ikkiga kopaytirganda chiqan javob ${ikkiga}`)
